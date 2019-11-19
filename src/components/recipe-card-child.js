37b998c2a9ef9d6729890b1eb6afc895cea3f075
phibalrath <52164530+phibalrath@users.noreply.github.com>
2019-11-19 08:43:54 -0500

    fixed image display

--- src/components/recipe-card-child.js
+++ src/components/recipe-card-child.js
@@ -13,13 +13,13 @@ class RecipeCardChild extends React.Component {
         return (<div>
             
             <div className="card">
-  <img src="{this.props.recipe.images}" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">{this.props.recipe.name}</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                <img src= {this.props.recipe.images} className="card-img-top" alt="Shirataki Ramen" />
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.recipe.name}</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
 
         </div>)
     }
