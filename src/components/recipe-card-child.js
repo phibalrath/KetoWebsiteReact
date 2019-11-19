d8375bc55737b24384344fa53b3e895bb8f4f6c6
phibalrath <52164530+phibalrath@users.noreply.github.com>
2019-11-19 11:02:31 -0500

    added container for cards

--- src/components/recipe-card-child.js
+++ src/components/recipe-card-child.js
@@ -11,17 +11,25 @@ class RecipeCardChild extends React.Component {
 
     render() {
         return (<div>
-            
-            <div className="card">
-                <img src= {this.props.recipe.images} className="card-img-top" alt="Shirataki Ramen" />
-                    <div className="card-body">
-                        <h5 className="card-title">{this.props.recipe.name}</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-            </div>
-
-        </div>)
+        
+       
+                <div className="col-sm"  d-flex align-items-stretch>
+                    <div className="card" style={{width: 285, height: 450}}>
+                        <center><img src= {this.props.recipe.images} style={{width: 285, height: 250}} className="card-img-top" alt="Shirataki Ramen" /></center>
+                            <div className="card-body">
+                                <h5 className="card-title">{this.props.recipe.name}</h5>
+                                <div className="card-text">
+                                    {this.props.recipe.numberOfIngredients} Ingredients |  
+                                    {this.props.recipe.cookTime} Minutes | 
+                                    {this.props.recipe.carbs} Carbs
+                                </div> 
+                             <br />
+                                <a href="#" className="btn btn-primary">Add to Meal Plan</a>
+                            </div>
+                    
+             </div>
+        </div>
+    </div>)
     }
 }
 
